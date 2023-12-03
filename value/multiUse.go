170387226c03e6117e367dd37649759c49e448b6
hneemann <helmut.neemann@arcor.de>
2023-12-03 12:14:15 +0100

    added some comments

--- value/multiUse.go
+++ value/multiUse.go
@@ -12,6 +12,58 @@ import (
 
 const startTimeout = 2 // seconds
 
+// MultiUse takes a map of closures and the list is passed to the closures. The
+// return values of the closures are returned in a map. The keys in the result
+// map are the same keys used to pass the closures. This is useful if you have to
+// use the same list multiple times and the list is both expensive to create and
+// expensive to store. This is because this method allows you to use the list
+// multiple times without having to store the list elements for later reuse.
+func (l *List) MultiUse(st funcGen.Stack[Value]) Map {
+	if m, ok := st.Get(1).ToMap(); ok {
+		var muList multiUseList
+		m.Iter(func(key string, value Value) bool {
+			if f, ok := value.ToClosure(); ok {
+				if f.Args == 1 {
+					muList = append(muList, &multiUseEntry{name: key, fu: f.Func})
+				} else {
+					panic("map in multiUse needs to contain closures with one argument")
+				}
+			} else {
+				panic("map in multiUse need to contain closures")
+			}
+			return true
+		})
+
+		started := make(chan struct{})
+		for _, mu := range muList {
+			mu.start(started)
+		}
+
+		// wait for all iterators to start
+		for i := 0; i < len(muList); i++ {
+			select {
+			case <-time.After(startTimeout * time.Second):
+				panic(muList.timeOutError())
+			case <-started:
+			}
+		}
+
+		go muList.run(l.Iterator())
+
+		resultMap := listMap.New[Value](len(muList))
+		for _, mu := range muList {
+			result := <-mu.result
+			if result.err != nil {
+				panic(result.err)
+			}
+			resultMap = resultMap.Append(mu.name, result.result)
+		}
+		return NewMap(resultMap)
+	} else {
+		panic("first argument in multiUse needs to be a map")
+	}
+}
+
 type multiUseEntry struct {
 	name         string
 	fu           funcGen.Func[Value]
@@ -113,50 +165,4 @@ func (ml multiUseList) timeOutError() error {
 		}
 	}
 	return errors.New(buffer.String())
-}
-
-func (l *List) MultiUse(st funcGen.Stack[Value]) Map {
-	if m, ok := st.Get(1).ToMap(); ok {
-		var muList multiUseList
-		m.Iter(func(key string, value Value) bool {
-			if f, ok := value.ToClosure(); ok {
-				if f.Args == 1 {
-					muList = append(muList, &multiUseEntry{name: key, fu: f.Func})
-				} else {
-					panic("map in multiUse needs to contain closures with one argument")
-				}
-			} else {
-				panic("map in multiUse need to contain closures")
-			}
-			return true
-		})
-
-		started := make(chan struct{})
-		for _, mu := range muList {
-			mu.start(started)
-		}
-
-		// wait for all iterators to start
-		for i := 0; i < len(muList); i++ {
-			select {
-			case <-time.After(startTimeout * time.Second):
-				panic(muList.timeOutError())
-			case <-started:
-			}
-		}
-
-		go muList.run(l.Iterator())
-
-		resultMap := listMap.New[Value](len(muList))
-		for _, mu := range muList {
-			result := <-mu.result
-			if result.err != nil {
-				panic(result.err)
-			}
-			resultMap = resultMap.Append(mu.name, result.result)
-		}
-		return NewMap(resultMap)
-	} else {
-		panic("first argument in multiUse needs to be a map")
-	}
 }
